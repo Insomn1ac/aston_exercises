1638fcfad5b77251463f3efb1bad384cbc4d7881
Insomn1ac <emelyanoff94@gmail.com>
2022-10-22 12:26:16 +0300

    Custom class was added

--- src/test/java/exercises/sort/MergeSortTest.java
+++ src/test/java/exercises/sort/MergeSortTest.java
@@ -2,31 +2,42 @@ package exercises.sort;
 
 import exercises.linkedlist.LinkedList;
 import exercises.linkedlist.LinkedListImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MergeSortTest {
+    private LinkedList<SimpleClass> inputList;
+
+    @BeforeEach
+    public void init() {
+        inputList = new LinkedListImpl<>();
+    }
+
+    @Test
+    public void whenTestingComparatorOfSimpleClass() {
+        SimpleClass first = new SimpleClass(1, "1");
+        SimpleClass second = new SimpleClass(2, "2");
+        assertThat(first.compareTo(second)).isLessThan(0);
+        assertThat(second.compareTo(first)).isGreaterThanOrEqualTo(1);
+    }
 
     @Test
     public void whenMergeSort() {
-        LinkedList<Integer> inputList = new LinkedListImpl<>();
-        inputList.add(3);
-        inputList.add(5);
-        inputList.add(1);
-        inputList.add(2);
-        inputList.add(4);
-        LinkedList<Integer> expected = new LinkedListImpl<>();
-        expected.add(1);
-        expected.add(2);
-        expected.add(3);
-        expected.add(4);
-        expected.add(5);
-        LinkedList<Integer> output = MergeSort.sort(inputList);
-        for (Integer el : output) {
-            System.out.println(el);
-        }
+        inputList.add(new SimpleClass(3, "3"));
+        inputList.add(new SimpleClass(5, "5"));
+        inputList.add(new SimpleClass(1, "1"));
+        inputList.add(new SimpleClass(2, "2"));
+        inputList.add(new SimpleClass(4, "4"));
+        LinkedList<SimpleClass> expected = new LinkedListImpl<>();
+        expected.add(new SimpleClass(1, "1"));
+        expected.add(new SimpleClass(2, "2"));
+        expected.add(new SimpleClass(3, "3"));
+        expected.add(new SimpleClass(4, "4"));
+        expected.add(new SimpleClass(5, "5"));
+        LinkedList<SimpleClass> output = MergeSort.sort(inputList);
         for (int i = 0; i < output.size(); i++) {
             assertEquals(expected.get(i), output.get(i));
         }
@@ -34,32 +45,29 @@ class MergeSortTest {
 
     @Test
     public void whenAllElementsInInputListAre1() {
-        LinkedList<Integer> inputList = new LinkedListImpl<>();
-        inputList.add(1);
-        inputList.add(1);
-        inputList.add(1);
-        LinkedList<Integer> expected = new LinkedListImpl<>();
-        expected.add(1);
-        expected.add(1);
-        expected.add(1);
-        LinkedList<Integer> output = MergeSort.sort(inputList);
+        inputList.add(new SimpleClass(1, "2"));
+        inputList.add(new SimpleClass(1, "1"));
+        inputList.add(new SimpleClass(1, "3"));
+        LinkedList<SimpleClass> expected = new LinkedListImpl<>();
+        expected.add(new SimpleClass(1, "1"));
+        expected.add(new SimpleClass(1, "2"));
+        expected.add(new SimpleClass(1, "3"));
+        LinkedList<SimpleClass> output = MergeSort.sort(inputList);
         assertEquals(expected.get(2), output.get(2));
     }
 
     @Test
     public void whenInputListLengthIs1() {
-        LinkedList<Integer> inputList = new LinkedListImpl<>();
-        inputList.add(3);
-        LinkedList<Integer> output = MergeSort.sort(inputList);
-        assertThat(output.get(0)).isEqualTo(3);
+        inputList.add(new SimpleClass(3, "3"));
+        LinkedList<SimpleClass> output = MergeSort.sort(inputList);
+        assertThat(output.get(0)).isEqualTo(new SimpleClass(3, "3"));
     }
 
     @Test
     public void whenInputListLengthIs2() {
-        LinkedList<Integer> inputList = new LinkedListImpl<>();
-        inputList.add(3);
-        inputList.add(1);
-        LinkedList<Integer> output = MergeSort.sort(inputList);
-        assertThat(output.get(1)).isEqualTo(3);
+        inputList.add(new SimpleClass(3, "3"));
+        inputList.add(new SimpleClass(1, "1"));
+        LinkedList<SimpleClass> output = MergeSort.sort(inputList);
+        assertThat(output.get(1)).isEqualTo(new SimpleClass(3, "3"));
     }
 }

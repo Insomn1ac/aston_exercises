ecefb5c7a54619658d3d93ed68e7d9b703c6cbe3
Insomn1ac <emelyanoff94@gmail.com>
2022-10-23 11:14:50 +0300

    Added some fixes due to pull request

--- src/main/java/exercises/sort/MergeSort.java
+++ src/main/java/exercises/sort/MergeSort.java
@@ -3,33 +3,36 @@ package exercises.sort;
 import exercises.linkedlist.LinkedList;
 import exercises.linkedlist.LinkedListImpl;
 
-public class MergeSort {
-    public static LinkedList<SimpleClass> sort(LinkedList<SimpleClass> list) {
+import java.util.Comparator;
+
+public class MergeSort<T> {
+    public LinkedList<T> sort(LinkedList<T> list, Comparator<T> comp) {
         if (list.size() == 1) {
             return list;
         }
         int middle = list.size() / 2;
-        LinkedList<SimpleClass> left = new LinkedListImpl<>();
-        LinkedList<SimpleClass> right = new LinkedListImpl<>();
+        LinkedList<T> left = new LinkedListImpl<>();
+        LinkedList<T> right = new LinkedListImpl<>();
         for (int i = 0; i < middle; i++) {
             left.add(list.get(i));
         }
         for (int i = middle; i < list.size(); i++) {
             right.add(list.get(i));
         }
-        sort(left);
-        sort(right);
-        return merge(left, right, middle, list.size() - middle);
+        sort(left, comp);
+        sort(right, comp);
+        return merge(left, right, middle, list.size() - middle, comp);
     }
 
-    private static LinkedList<SimpleClass> merge(LinkedList<SimpleClass> leftList,
-                                     LinkedList<SimpleClass> rightList,
-                                     int left, int right) {
+    private LinkedList<T> merge(LinkedList<T> leftList,
+                                LinkedList<T> rightList,
+                                int left, int right, Comparator<T> comp) {
         int indexLeft = 0;
         int indexRight = 0;
-        LinkedList<SimpleClass> outputList = new LinkedListImpl<>();
+        LinkedList<T> outputList = new LinkedListImpl<>();
         while (indexLeft < left && indexRight < right) {
-            if (leftList.get(indexLeft).compareTo(rightList.get(indexRight)) <= 0) {
+            int compareRsl = comp.compare(leftList.get(indexLeft), rightList.get(indexRight));
+            if (compareRsl < 0) {
                 outputList.add(leftList.get(indexLeft++));
             } else {
                 outputList.add(rightList.get(indexRight++));
--- src/test/java/exercises/sort/MergeSortTest.java
+++ src/test/java/exercises/sort/MergeSortTest.java
@@ -4,16 +4,23 @@ import exercises.linkedlist.LinkedList;
 import exercises.linkedlist.LinkedListImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.Comparator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MergeSortTest {
     private LinkedList<SimpleClass> inputList;
+    private Comparator<SimpleClass> comparator;
+    private Comparator<SimpleClass> compByName;
+    private MergeSort<SimpleClass> sort;
 
     @BeforeEach
     public void init() {
         inputList = new LinkedListImpl<>();
+        sort = new MergeSort<>();
+        comparator = SimpleClass::compareTo;
+        compByName = Comparator.comparing(SimpleClass::getName);
     }
 
     @Test
@@ -37,7 +44,7 @@ class MergeSortTest {
         expected.add(new SimpleClass(3, "3"));
         expected.add(new SimpleClass(4, "4"));
         expected.add(new SimpleClass(5, "5"));
-        LinkedList<SimpleClass> output = MergeSort.sort(inputList);
+        LinkedList<SimpleClass> output = sort.sort(inputList, comparator);
         for (int i = 0; i < output.size(); i++) {
             assertEquals(expected.get(i), output.get(i));
         }
@@ -52,14 +59,14 @@ class MergeSortTest {
         expected.add(new SimpleClass(1, "1"));
         expected.add(new SimpleClass(1, "2"));
         expected.add(new SimpleClass(1, "3"));
-        LinkedList<SimpleClass> output = MergeSort.sort(inputList);
+        LinkedList<SimpleClass> output = sort.sort(inputList, comparator.thenComparing(compByName));
         assertEquals(expected.get(2), output.get(2));
     }
 
     @Test
     public void whenInputListLengthIs1() {
         inputList.add(new SimpleClass(3, "3"));
-        LinkedList<SimpleClass> output = MergeSort.sort(inputList);
+        LinkedList<SimpleClass> output = sort.sort(inputList, comparator);
         assertThat(output.get(0)).isEqualTo(new SimpleClass(3, "3"));
     }
 
@@ -67,7 +74,7 @@ class MergeSortTest {
     public void whenInputListLengthIs2() {
         inputList.add(new SimpleClass(3, "3"));
         inputList.add(new SimpleClass(1, "1"));
-        LinkedList<SimpleClass> output = MergeSort.sort(inputList);
+        LinkedList<SimpleClass> output = sort.sort(inputList, comparator);
         assertThat(output.get(1)).isEqualTo(new SimpleClass(3, "3"));
     }
 }
